4f2467be0e2e07df231d392049686cfe1bf11cf2
Carolina Preuss <Carolina Preuss@LAPTOP-M0053HIS>
2019-05-02 13:18:05 +0200

    Namngav service.

--- TSB100UserProfileService/UserProfileService.svc.cs
+++ TSB100UserProfileService/UserProfileService.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace TSB100UserProfileService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserProfileService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select UserProfileService.svc or UserProfileService.svc.cs at the Solution Explorer and start debugging.
+    public class UserProfileService : IUserProfileService
     {
         public string GetData(int value)
         {
